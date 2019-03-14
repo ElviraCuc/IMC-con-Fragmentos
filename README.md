bbfebd58296c40fcbb0667379e07b45d91e1652d
scarlett gomez pacheco <Scarlett@DESKTOP-T8UTEIG.DESKTOP-T8UTEIG>
2019-03-14 00:41:52 -0600

    IMCApp con fragmentos

---
+++ README.md
@@ -0,0 +1,1 @@
+# Aplicacion IMCApp con Fragmentos.
---
+++ app/src/main/java/com/elviracuc/imcconfragmentos/MainActivity.java
@@ -0,0 +1,27 @@
+package com.elviracuc.imcconfragmentos;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //ed600 ADATA DURABLE
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if(fragment == null){
+            fragment = new imcFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
+
